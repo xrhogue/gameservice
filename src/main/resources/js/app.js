f28ae13c0437d68f79d6869930cf97ee01d46abd
xrhogue <xrogue@comcast.net>
2014-10-30 17:09:25 -0600

    added functionality to add a new stat

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -21,9 +21,9 @@
 		};
 	});
 	
-	app.controller('StatsController', ['$scope', '$modal', '$log', 'StatService', function($scope, $modal, $log, statService) {
-		var cellTemplate='<div class="ngCellText" data-ng-model="row"><a data-ng-click="updateSelectedRow(row,$event)"><img alt="Edit" src="edit.png" height="16px" width="16px"/></a></div>';
-		$scope.stats = statService.query();
+	app.controller('StatsController', ['$scope', '$modal', '$log', '$window', 'StatService', function($scope, $modal, $log, $window, StatService) {
+		var cellTemplate='<div class="ngCellText" data-ng-model="row"><a data-ng-click="updateSelectedRow(row,$event)"><img alt="Edit" src="edit.png" height="16px" width="16px"/></a><a data-ng-click="deleteSelectedRow(row,$event)"><img alt="Delete" src="delete.png" height="16px" width="16px"/></a></div>';
+		$scope.stats = StatService.query();
 		
 //		$scope.$on('ngGridEventEndCellEdit', function(evt) {
 //            statService.saveStat(evt.targetScope.row.entity);
@@ -36,11 +36,28 @@
 				             {field: 'shortForm', displayName: 'Short Name'},
 				             {field: 'longForm', displayName: 'Long Name'},
 				             {field: 'multiplier', displayName: 'Multiplier', cellClass: 'gridCellNumberStyle'},
-				             {field: '', cellTemplate: cellTemplate, width: '32px'}]
+				             {field: '', cellTemplate: cellTemplate, width: '48px'}]
 		};
 		
-		var dialog;
-		    
+		$scope.addNewAttribute = function() {
+			var entity = new StatService();
+			
+			$scope.stats.push(entity);
+			
+		    var modalInstance = $modal.open({
+		    	templateUrl: 'statDlg.html',
+		    	controller: ModalInstanceCtrl,
+		    	resolve: {
+		    		item: function () {
+		    			return entity;
+		    		},
+		    		stats: function () {
+		    			return $scope.stats;
+		    		}
+		    	}
+		    });			
+		};
+		
 		$scope.updateSelectedRow = function(row) {
 			$scope.myrow = row.entity;
 			
@@ -50,13 +67,27 @@
 		    	resolve: {
 		    		item: function () {
 		    			return row.entity;
+		    		},
+		    		stats: function () {
+		    			return $scope.stats;
 		    		}
 		    	}
 		    });
 		};
+	    
+		$scope.deleteSelectedRow = function(row) {
+			$scope.selectedItem = row.entity;
+			
+			var deleteItem = $window.confirm('Delete ' + $scope.selectedItem.shortForm + '?');
+			
+			if (deleteItem) {				
+				$scope.stats.splice(row.rowIndex, 1);
+				$scope.selectedItem.$delete({verb: 'delete', id: $scope.selectedItem.id});
+			}
+		};
 	}]);
 	
-	app.directive('uppercase', function() {
+	app.directive('uppercase', ['$parse', function($parse) {
 		return {
 			restrict: 'A',
 			require: 'ngModel',
@@ -84,31 +115,120 @@
 		        uppercase($parse(attrs.ngModel)(scope));  // capitalize initial value
 		    }
 		};
-	});
+	}]);
 	
-	var ModalInstanceCtrl = function ($scope, $modalInstance, item) {
-		  $scope.item = item;
+	app.directive('capitalize', ['$parse', function($parse) {
+		return {
+			restrict: 'A',
+			require: 'ngModel',
+		    link: function(scope, element, attrs, modelCtrl) {
+		    	var capitalize = function(inputValue) {
+		           if (inputValue == undefined)
+		        	   inputValue = '';
+		           		           
+		           var capitalized = inputValue;
+		           
+		           if (inputValue.length > 0) {
+		        	   capitalized = inputValue.substr(0, 1).toUpperCase();
+		        	   
+		        	   if (inputValue.length > 1) {
+		        		   capitalized += inputValue.substr(1, inputValue.length - 1).toLowerCase();
+		        	   }
+		           }
+		           
+		           if (attrs.ngMaxlength != undefined && uppercased.length > attrs.ngMaxlength) {
+		        	   capitalized = capitalized.substr(0, attrs.ngMaxlength);
+		           }
+		           
+		           if (capitalized !== inputValue) {
+		        	   modelCtrl.$setViewValue(capitalized);
+		        	   modelCtrl.$render();
+		           }
+		           
+		           return capitalized;
+		    	}
+		    	
+		        modelCtrl.$parsers.push(capitalize);
+		         
+		        capitalize($parse(attrs.ngModel)(scope));  // capitalize initial value
+		    }
+		};
+	}]);
+	
+	app.directive('checkRange', ['$parse', function($parse) {
+		return {
+			restrict: 'A',
+			require: 'ngModel',
+		    link: function(scope, element, attrs, modelCtrl) {
+		    	var getValue = function(inputValue) {
+		           if (inputValue == undefined)
+		        	   inputValue = attrs.ngMin;
+		           
+		           value = inputValue;
+		           
+		           if (attrs.ngMin != undefined && value < attrs.ngMin) {
+		        	   value = attrs.ngMin;
+		           }
+		           
+		           if (attrs.ngMax != undefined && value > attrs.ngMax) {
+		        	   value = attrs.ngMax;
+		           }
+		           
+		           if (value !== inputValue) {
+		        	   modelCtrl.$setViewValue(value);
+		        	   modelCtrl.$render();
+		           }
+		           
+		           return value;
+		    	}
+		    	
+		        modelCtrl.$parsers.push(getValue);
+		         
+		        getValue($parse(attrs.ngModel)(scope));  // force value into range
+		    }
+		};
+	}]);
+	
+	var ModalInstanceCtrl = function ($scope, $modalInstance, item, stats) {
+		$scope.availableCodes = getAvailableCodes(stats);
+		$scope.item = item;
 		  
-		  var id = item.id;
-		  var code = item.code;
-		  var shortForm = item.shortForm;
-		  var longForm = item.longForm;
-		  var multiplier = item.multiplier;
+		var id = item.id;
+		var code = item.code;
+		var shortForm = item.shortForm;
+		var longForm = item.longForm;
+		var multiplier = item.multiplier;
 
-		  $scope.ok = function () {
-			  item.$save({verb: 'update', stat: item})
+		$scope.ok = function () {
+			item.$save({verb: 'update', stat: item})
 			  
-			  $modalInstance.close(item);
-		  };
+			$modalInstance.close(item);
+		};
 
-		  $scope.cancel = function () {
-			  item.id = id;
-			  item.code = code;
-			  item.shortForm = shortForm;
-			  item.longForm = longForm;
-			  item.multiplier = multiplier;
-			  
-			  $modalInstance.dismiss('cancel');
-		  };
+		$scope.cancel = function () {
+			item.id = id;
+			item.code = code;
+			item.shortForm = shortForm;
+			item.longForm = longForm;
+			item.multiplier = multiplier;
+			
+			if (item.id == undefined) {
+				stats.pop();
+			}
+			
+			$modalInstance.dismiss('cancel');
+		};
+		
+		function getAvailableCodes(stats) {
+			var availableCodes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+			
+			stats.forEach(function(stat) {
+				if (availableCodes.indexOf(stat.code) > -1 && item.code != stat.code) {
+					availableCodes.splice(availableCodes.indexOf(stat.code), 1);
+				}
+			});
+			
+			return availableCodes;
+		}
 	};
 })();
